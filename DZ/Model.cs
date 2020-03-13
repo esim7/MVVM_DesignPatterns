0e2e150915b8850b958acef6f81a33a715e23b0c
esim7 <makishev@yahoo.com>
2020-03-13 17:53:47 +0600

    created

---
+++ DZ/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ
+{
+    public class Model
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsMale { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
